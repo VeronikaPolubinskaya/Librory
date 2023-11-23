876cac5e75989335f3b0df49c40c9c8dff2548a5
Ви <veronikafeliz.by@gmail.com>
2023-11-23 19:45:35 +0300

    undo change secret settings

--- library/library/settings.py
+++ library/library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#####################'
+SECRET_KEY = 'django-insecure-jkm5r8ws7va3$5d_t1@ca_9z)(!_aihrrv#r1zfz35p(wq312+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'library',
         'USER': 'user1',
-        'PASSWORD': '######',
+        'PASSWORD': 'user2023',
         'HOST': 'localhost',
         'PORT': 3306,
     }
@@ -135,7 +135,7 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "yuormail@gmail.com"
-EMAIL_HOST_PASSWORD = '#### #### #### ####'
+EMAIL_HOST_USER = "searchmp2@gmail.com"
+EMAIL_HOST_PASSWORD = 'ores gcvm rvfb unsl'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
